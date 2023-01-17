d39fd2a28dc4da995fd7a570120864dc460af239
Sushil Behera <sbehera@mpgbis.com>
2023-01-17 19:43:18 +0530

    delete email pages and update sitemap and vercel jason

--- pages/_app.js
+++ pages/_app.js
@@ -25,26 +25,14 @@ function MyApp({ Component, pageProps }) {
       router.asPath =='/contact-us/?utm_source=email&utm_medium=contact-us&utm_campaign=navtobc_jan&utm_term=ds_us' ||
       router.asPath =='/contact-us/?utm_source=email&utm_medium=contact-us&utm_campaign=business_central_support&utm_term=ds_us' ||
       router.asPath =='/subscriber-thank-you/' ||
-      router.asPath =='/email-dynamics365-crm/' ||
-      router.asPath =='/emailer-power-bi/' ||
       router.asPath =='/not-for-profit-fundrasing-engagenment/' ||
-      router.asPath =='/email-ax-to-finance/' ||
-      router.asPath =='/email-gp-to-bc/' ||
-      router.asPath =='/email-wms/' ||
       router.asPath == '/thank-you/email-subscriber/' ||
       router.asPath == '/subscribe-updates/' ||
       router.asPath == '/subscribe-updates/?utm_source=email&utm_medium=subscribe_updates&utm_campaign=business_central_support&utm_term=ds_us' ||
       router.asPath == '/subscribe-updates/?utm_source=email&utm_medium=subscribe_updates&utm_campaign=navtobc_jan&utm_term=ds_us' ||
-      router.asPath == '/support-dynamics-365/' ||
-      router.asPath == '/support-dynamics-365/#' ||
-      router.asPath == '/support-dynamics-365/?utm_source=email&utm_medium=banner&utm_campaign=business_central_support&utm_term=ds_us' ||
-      router.asPath == '/support-dynamics-365/?utm_source=Email&utm_medium=navtobc_campaign&utm_campaign=business_central_support&utm_term=us' ||
       router.asPath == '/thank-you/' ||
       router.asPath == '/thanks-webinar-registration/' ||
-      router.asPath == '/upgrade-nav-to-business-central/' ||
-      router.asPath == '/upgrade-nav-to-business-central/#' ||
-      router.asPath == '/upgrade-nav-to-business-central/?utm_source=Email&utm_medium=navtobc_campaign&utm_campaign=upgrade_nav_to_business_central&utm_term=us' ||
-      router.asPath == '/dynamics-nav-support-ending-soon/'
+      router.asPath == '/dynamics-nav-support-ending-soon/' 
       
   )  {
     return (
