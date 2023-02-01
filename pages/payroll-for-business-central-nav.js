296a22b67fb1d5bb093d7543f49dd32d6c311584
Sushil Behera <sbehera@mpgbis.com>
2023-02-01 19:11:45 +0530

    add new page payroll BC

---
+++ pages/payroll-for-business-central-nav.js
@@ -0,0 +1,301 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs, Autoplay } from "swiper";
+const PowerBIImplementation = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  return (
+    <>
+      <Head>
+        <title>NaviPayroll: HR Payroll for Business Central | Dynamics NAV</title>
+        <meta
+          name="description"
+          content="NaviPayroll is a popular HR Payroll add-on from Serenic software for Microsoft Dynamics 365 Business Central. Get in touch for FREE Demo and Pricing. "
+        />
+        <link
+          rel="canonical"
+          href="https://www.dynamicssquare.com/payroll-for-business-central-nav/"
+        />
+      </Head>
+
+      <section className="Solution-banner hero-1 hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mobile-4 align-self-center">
+              <h1>HR Payroll Add-on for Dynamics 365 Business Central</h1>
+              <p>Integrate <b>NaviPayroll Add-On</b> to Your Seamless Business Management Application: Microsoft Dynamics 365 Business Central and Get Started!</p>
+              <div>
+                <div className="text-center text-lg-start">
+                  <Link href="#exampleModal">
+                    <a
+                      data-bs-toggle="modal"
+                      className="btn-get-started scrollto"
+                    >
+                      <span>Get Rid of Payroll Problems Now!</span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <img src="/img/Payroll-for-Business-Central.png" alt="Payroll-for-Business-Central" width={864} height={500} />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <header className="section-header">
+                <h2>Simplify and Streamline Your Payroll and HR Management at Scale with NaviPayroll!</h2>
+                <p>Say good-bye to <b>tedious payroll</b> and <b>HR processing</b>, and welcome boosted accuracy, and all-time compliance with an all-in-one payroll solution - NaviPayroll for <Link href="/products/dynamics-365-business-central/"><a>Business Central ERP</a></Link>, which is designed & developed to suit businesses across all industries.</p>
+                <p>From manufacturing, services, distribution, and retail to the public sector, NaviPayroll has you covered.</p>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="busine_service_bottom custom05">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5 align-self-center">
+              <div className="busine_service_bottom-left custom012">
+                <Image
+                  src="/img/NaviPayroll-Dynamics-365-Business-Central.png"
+                  alt="NaviPayroll-Dynamics-365-Business-Central"
+                  width={625} height={651}
+                />
+              </div>
+            </div>
+            <div className="col-lg-1"></div>
+            <div className="col-lg-6 align-self-center">
+              <div className="side-heading-custom-01">
+                <h2>NaviPayroll + Dynamics 365 Business Central = Easy to Use & Fully Integrated Payrolls Solutions</h2>
+              </div>
+              <div className="custom-p-001">
+                <p>Most secure and strong Payment infrastructure for all your online transactions.</p>
+                <p>Businesses of all sizes, from <b>small to medium size</b> and <b>large enterprises</b> - use <a href="https://www.sylogist.com/support-legacy" target="_blank" rel="nofollow">NaviPayroll</a>; designed and developed by <a href="https://www.sylogist.com/" target="_blank" rel="nofollow">Sylogist</a> and is made available by <a href="https://www.serenic.com/" target="_blank" rel="nofollow">Serenic</a>. It is a payments add-on for the all-in-one <b>cloud-based business management solution from Microsoft - D365 Business Central</b>.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      
+
+      <section className="busine_service_bottom">
+        <div className="container">
+          <div className="row" style={{paddingBottom:'80px'}}>
+            <div className="col-lg-10 align-self-center">
+              <div className="side-heading-custom-01">
+                <h3>Why Choose NaviPayroll for Dynamics 365 Business Central?</h3>
+              </div>
+              <div className="custom-p-001">
+                <p>NaviPayroll is an advanced <b>Payroll Add-On</b> for <b>D365 Business Central</b>, and this combination offers a seamless solution for all your payroll needs, benefits include:</p>
+              </div>
+              <ul>
+                <li>Automated payroll processing</li> 
+                <li>Real-time data integration</li> 
+                <li>Improved accuracy & efficiency</li> 
+                <li>Secure & compliant data management </li>
+              </ul>
+            </div>
+          
+          </div>
+          <div className="row">
+            <div className="col-lg-6 align-self-center">
+              <div className="side-heading-custom-01">
+                <h3>Who can choose NaviPayroll?</h3>
+              </div>
+              <div className="custom-p-001">
+                <p><a href="https://blog.dynamicssquare.com/dynamics-365-business-central-key-for-2023/">Small and medium-size businesses</a> to large international groups can opt for this easy and highly secure payments gateway suite that seamlessly <b>integrates with Dynamics 365 Business Central</b> and helps you avoid all mindless tasks.</p>
+              </div>
+              
+            </div>
+
+            <div className="col-lg-5 align-self-center">
+              <div className="busine_service_bottom-left custom012">
+                <img
+                  src="/img/NaviPayroll-side.png"
+                  alt="NaviPayroll-side"
+                />
+                <p style={{marginTop:'10px',fontWeight:'600',fontSize:'14px'}}>Dynamics 365 Business Central's Payroll UI (User Interface) After NaviPayroll  Integration </p>
+              </div>
+            </div>
+            <div className="col-lg-1 align-self-center"></div>
+          
+           
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <header className="section-header">
+                <h4>Which Platforms are right for NaviPayroll? </h4>
+                <p>NaviPayroll is a web-based add-on for Microsoft Business Central.</p>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="call-to-action-center hero-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="call-to-action-content">
+                <h3>Help Your Teams Get started with NaviPayroll and experience a hassle-free payroll process from day one!</h3>
+                <div className="text-center m-o-t new-btn-nn">
+                  <Link href="#exampleModal">
+                    <a
+                      data-bs-toggle="modal"
+                      className="btn-get-started scrollto"
+                    >
+                      <span>Contact our Business Central Support team now!</span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="solution-faq faq">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9 col-md-9">
+              <header className="section-header">
+                <h2>FAQs for You: NaviPayroll and Dynamics 365 Business Central Integration </h2>
+              </header>
+            </div>
+          </div>
+          <div className="row justify-content-center mar-top-7">
+            <div className="col-lg-8">
+              <div className="accordion accordion-flush" id="faqlist1">
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#faq-content-1"
+                    >Can I integrate NaviPayroll in my Dynamics NAV?</button>
+                  </h3>
+                  <div
+                    id="faq-content-1"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#faqlist1"
+                  >
+                    <div className="accordion-body">
+                      <p>Yes. Whether you're running your business operations using <Link href="/products/microsoft-dynamics-nav/"><a>Dynamics NAV/Navision</a></Link> or <b>D365 Business Central</b>, you can <b>integrate</b> it to easily automate all your manual payroll processes.</p>
+                      <p><Link href="/contact-us/"><a>Contact us to get a quote</a></Link></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#faq-content-2"
+                    >Is it necessary to upgrade my legacy ERP system like Dynamics NAV to Business Central to use NaviPayroll?</button>
+                  </h3>
+                  <div
+                    id="faq-content-2"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#faqlist1"
+                  >
+                    <div className="accordion-body">
+                      <p>No. Not necessarily. However, you can upgrade your disparate or outgrown ERP systems like <Link href="/dynamics-nav-to-business-central/"><a>Dynamics NAV to Dynamics 365 Business Central</a></Link>, which allows integrations from various third-party apps to help you accelerate your business ops.</p>
+                      <p><b>Contact us</b> for simplified and cost-effective <Link href="/dynamics-365-business-central-implementation/"><a>Dynamics 365 Business Central Implementation</a></Link>. Follow this link for pricing and licensing of Business Central - <b>Affordable</b> <Link href="/dynamics-365-business-central-pricing-licensing/"><a>Pricing and Licensing for Dynamics 365 Business Central</a></Link>. </p>
+                      <p><Link href="/schedule-a-demo/"><a>Feel free to contact us to get a demo</a></Link></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#faq-content-3"
+                    >Does Microsoft business central include payroll?</button>
+                  </h3>
+                  <div
+                    id="faq-content-3"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#faqlist1"
+                  >
+                    <div className="accordion-body">
+                      <p><b>D365 Business Central</b> has basic capabilities, so you need a <b>payroll Module</b> like <b>NaviPayroll</b> helps users increase their productivity, reduce administrative workloads and improve accuracy.</p>
+                      <p>It is an <b>add-on from Sylogist</b> and it gets easily <b>integrated with</b> <Link href="/products/dynamics-365-business-central/"><a>Microsoft Business Central</a></Link> and <b>Dynamics NAV</b> to help you avoid manual data entry tasks and start enjoying real-time data exchange between your payroll and financial systems.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#faq-content-4"
+                    >How is the accuracy and compliance of NaviPayroll?</button>
+                  </h3>
+                  <div
+                    id="faq-content-4"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#faqlist1"
+                  >
+                    <div className="accordion-body">
+                      <p><b>NaviPayroll's integration</b> with <b>D365 Business Central</b> ensures full data accuracy and helps you stay compliant with all regulations. Once you have <b>integrated NaviPayroll</b> to your <b>Business Central ERP</b>, there is no need to worry about any sort of online or in-person payment issues.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h3 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#faq-content-5"
+                    >What is the cost of NaviPayroll's integration with Dynamics 365 Business Central?</button>
+                  </h3>
+                  <div
+                    id="faq-content-5"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#faqlist1"
+                  >
+                    <div className="accordion-body">
+                      <p>It is difficult to say the specific <b>cost of NaviPayroll integration</b> with Business Central as it may vary depending on several factors such as the specific configuration and custom requirements, the vendor, and the region. It's best to <b>contact NaviPayroll</b> or a <b>Microsoft Partner</b> like <b>Dynamics Square</b> for a detailed and accurate quote.</p>
+                      <p><Link href="/"><a>You can find an overview of Microsoft Gold Partners here </a></Link></p>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  );
+};
+
+export default PowerBIImplementation;
