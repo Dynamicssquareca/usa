56a4cc38b1270a7dcbe36980dbc6d5a645287db8
Sushil Behera <sbehera@mpgbis.com>
2023-06-16 13:08:28 +0530

    update css and add class

--- pages/microsoft-dynamics-365-partner.js
+++ pages/microsoft-dynamics-365-partner.js
@@ -226,15 +226,15 @@ const MicrosoftDynamicsPartnerUsa = () => {
                   Applications
                 </h2>
               </header>
+              <div className="custom-ifram-size">
               <iframe
-                width="800"
-                height="400"
                 src="https://www.youtube.com/embed/mO3-PLN32Rg?start=1"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen
               ></iframe>
+              </div>
             </div>
           </div>
         </div>
