68e4cb7ba5679022ac24f849a00103e7fba7dea4
Sushil Behera <sbehera@mpgbis.com>
2023-12-27 15:20:33 +0530

    updat

--- pages/_document.js
+++ pages/_document.js
@@ -92,6 +92,7 @@ class MyDocument extends Document {
           <NextScript />
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
           <script src="//code.jivosite.com/widget/UMeAIYNyyq" async></script>
+          
         </body>
       </Html>
     )
