d73023120e2493b20995762b0b3960d20eba179b
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-07-30 16:41:05 +0530

    update copilate

--- pages/white-papers/micorosoft-copilot-for-transformation-in-your-business.js
+++ pages/white-papers/micorosoft-copilot-for-transformation-in-your-business.js
@@ -1,53 +1,138 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState,useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Script from "next/script";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
 import emailjs from "@emailjs/browser";
-import { Autoplay, FreeMode, Pagination, Navigation } from "swiper";
-const StartGuideOne = () => {
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+const Whitepaerguide = () => {
+
   const router = useRouter();
-  const [display, setDisplay] = useState("dspn");
-  const form = useRef();
+  const [display, setDisplay] = useState(false);
   const [closeModal, setCloseModal] = useState(false);
-  function handleCloseModal() {
-    document.getElementById("exampleModal").classList.remove("show", "d-block");
-    document
-      .querySelectorAll(".modal-backdrop")
-      .forEach((el) => el.classList.remove("modal-backdrop"));
-  }
-  const sendEmail = (e) => {
-    setDisplay("spinner-border text-success");
-    e.preventDefault();
+  const [errors, setErrors] = useState({});
+  const [defaultCountryCode, setDefaultCountryCode] = useState('us'); 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    company_name: '',
+    phone: '',
+  });
+  const form = useRef();
 
-    emailjs
-      .sendForm(
-        "service_cxxhqoq",
-        "template_f80fich",
-        form.current,
-        "7Ae4EIVY__86NsTQD"
-      )
 
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+/*auto fetch*/
+useEffect(() => {
+  // Fetch IP information when the component mounts
+  fetchCountryCodeByIP();
+}, []);
 
-    setTimeout(function () {
-      setCloseModal(true);
-      e.target.reset();
-      router.push("https://www.dynamicssquare.com/white-papers/download/pdf-micorosoft-copilot/");
-    }, 500);
+const fetchCountryCodeByIP = () => {
+  fetch(`https://api.ipdata.co?api-key=6047ffae1867788acd06e6870e8ee7a52268aa63e892e74e80b56b66`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch IP information');
+      }
+      return response.json();
+    })
+    .then(data => {
+      let countryCode = data.country_code.toLowerCase(); 
+      console.log("Country Code:", countryCode); // 
+      setDefaultCountryCode(countryCode);
+      console.log("Default Country Code:", defaultCountryCode); 
+    })
+    .catch(error => {
+      console.error('Error fetching IP information:', error);
+      setDefaultCountryCode('us');
+    });
+};
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.name) {
+      newErrors.name = 'Full Name is required';
+    }
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!live.com)(?!outlook.com)[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]{2,61}$/;
+    if (!emailPattern.test(formData.email)) {
+      newErrors.email = 'Invalid email address';
+    }
+    if (!formData.company_name) {
+      newErrors.company_name = 'Company Name is required';
+    }
+    if (!formData.phone.trim()) {
+      errors.phone = 'Phone number is required';
+    } else if (!isValidPhoneNumber(formData.phone)) {
+      errors.phone = 'Invalid phone number';
+    }
+    if (!formData.phone) {
+      newErrors.phone = 'Phone Number is required';
+    } else {
+      const phonePattern = /^\d{10,13}$/;
+      if (!phonePattern.test(formData.phone)) {
+        newErrors.phone = 'Invalid phone number';
+      }
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    // Clear the error for the field being edited
+    if (errors[name]) {
+      setErrors({
+        ...errors,
+        [name]: '',
+      });
+    }
+  };
+
+  const handlePhoneChange = (phone) => {
+    setFormData({ ...formData, phone });
+    // Clear error message for the phone field
+    setErrors((prevErrors) => ({ ...prevErrors, phone: '' }));
+  };
+
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    if (!validate()) {
+      return;
+    }
+
+    setDisplay(true);
+
+
+    try {
+      const result = await emailjs.sendForm('service_cxxhqoq', 'template_f80fich', form.current, '7Ae4EIVY__86NsTQD');
+      console.log(result.text);
+      setFormData({
+        name: '',
+        email: '',
+        company_name: '',
+        phone: '',
+      });
+      router.push("https://www.dynamicssquare.com/white-papers/download/pdf-micorosoft-copilot/");
+    } catch (error) {
+      console.error(error.text);
+    } finally {
+      setDisplay(false);
+    }
+  };
+
+  const isValidPhoneNumber = (phone) => {
+    // Phone number should be between 10 to 13 characters
+    return /^\d{10,15}$/.test(phone);
+  };
+
+
 
   return (
     <>
@@ -214,15 +299,18 @@ const StartGuideOne = () => {
               <p>Fill the form and receive it straight to your inbox.</p>
               <div className="modal-body">
                 <div className="main-form-wrper">
-                  <form ref={form} onSubmit={sendEmail}>
+
+                <form ref={form} onSubmit={sendEmail}>
                     <div className="mb-3">
                       <input
                         type="text"
                         className="form-control"
                         placeholder="*Full Name"
                         name="name"
-                        required
+                        value={formData.name}
+                        onChange={handleChange}
                       />
+                      {errors.name && <span className="error">{errors.name}</span>}
                       <input type="hidden" value={router.asPath} name="url" />
                     </div>
 
@@ -232,9 +320,10 @@ const StartGuideOne = () => {
                         className="form-control"
                         placeholder="*Work Email"
                         name="email"
-                        pattern="^[a-zA-Z0-9._%+-]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!aol.com)(?!live.com)(?!outlook.com)[a-zA-Z0-9_-]+.[a-zA-Z0-9-.]{2,61}$"
-                        required
+                        value={formData.email}
+                        onChange={handleChange}
                       />
+                      {errors.email && <span className="error">{errors.email}</span>}
                     </div>
                     <div className="mb-3">
                       <input
@@ -242,53 +331,84 @@ const StartGuideOne = () => {
                         className="form-control"
                         placeholder="*Company Name"
                         name="company_name"
-                        required
+                        value={formData.company_name}
+                        onChange={handleChange}
                       />
+                      {errors.company_name && <span className="error">{errors.company_name}</span>}
                     </div>
                     <div className="mb-3">
-                      <input
+                    <PhoneInput inputStyle={{ width: '100%', height: 'auto' }}
+                        country={defaultCountryCode} // Set default country code
+                        value={formData.phone}
+                        onChange={handlePhoneChange}
+                        inputClass="form-control" // CSS class for the input
+                        inputProps={{
+                          name: 'phone',
+                          required: true,
+                          autoFocus: false,
+                          onBlur: () => {
+                            if (formData.phone.trim() !== '') { // Check if phone number is not empty before validation
+                              if (!isValidPhoneNumber(formData.phone)) {
+                                setErrors({ ...errors, phone: 'Invalid phone number format' });
+                              } else {
+                                delete errors.phone; // Clear error if phone number is valid
+                              }
+                            } else {
+                              delete errors.phone; // Clear error if phone number is empty
+                            }
+                          }
+                        }}
+                        countryCodeEditable={false}
+                        // onlyCountries={['us', 'ca', 'mx', 'gb']}
+                        excludeCountries={['pk']}
+                      />
+                      {errors.phone && <span className="error">{errors.phone}</span>}
+                      {/* <input
                         type="tel"
                         className="form-control"
-                        placeholder="*Phone Number"
+                        placeholder="Phone Number"
                         name="phone"
-                        pattern="^\d{10,13}$"
-                        required
+                        value={formData.phone}
+                        onChange={handleChange}
                       />
+                      {errors.phone && <span className="error">{errors.phone}</span>} */}
                     </div>
+                   
                     <div className="mb-3 form-check">
                       <input
                         type="checkbox"
                         checked
                         readOnly
                         className="form-check-input"
-                        id="exampleCheck1"
                       />
-                      <label
-                        className="form-check-label"
-                        htmlFor="exampleCheck1"
-                      >
+                      <label className="form-check-label">
                         I agree to the
-                        <Link href="/privacy-policy/" target="_blank">
-                          Privacy Policy
-                        </Link>
+                        <a href="/privacy-policy/" target="_blank">
+                          {" "}
+                          Privacy Policy{" "}
+                        </a>
                         and
-                        <Link href="/terms-of-use/" target="_blank">
-                          Terms of Service
-                        </Link>
+                        <a href="/terms-of-use/" target="_blank">
+                          {" "}
+                          Terms of Service{" "}
+                        </a>
                         .
                       </label>
                     </div>
 
-                    <div className="spiner-wrper">
+                    <div className="spinner-wrapper">
                       <button
                         type="submit"
                         className="btn btn-primary fomr-submit"
+                        disabled={display}
                       >
-                        Download Now
+                        {display ? 'Submitting...' : 'Submit'}
                       </button>
-                      <div className={display} role="status">
-                        <span className="visually-hidden">Loading...</span>
-                      </div>
+                      {display && (
+                        <div className="spinner-border text-success" role="status">
+                          <span className="visually-hidden">Loading...</span>
+                        </div>
+                      )}
                     </div>
                   </form>
                 </div>
@@ -301,4 +421,4 @@ const StartGuideOne = () => {
   );
 };
 
-export default StartGuideOne;
+export default Whitepaerguide;
