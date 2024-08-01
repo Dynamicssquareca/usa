87975e645eae4ceb76d694a97bd9b662acd34b94
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-08-01 20:37:18 +0530

    update page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -48,7 +48,7 @@ export async function getServerSideProps() {
 
 function Blogshome({ blogs, businesscentral, categoryblogs, blograndomblogs, blogtranding }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 5;
+  const pageSize = 15;
 
   const onPageChange = (page) => {
     setCurrentPage(page);
